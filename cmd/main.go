e5ef9f42cc0ba381f76681524a7da4991ab87518
Alejandro Hernandez <alejandrojaez@outlook.com>
2024-01-23 19:46:06 -0600

    upd

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	cp "github.com/otiai10/copy"
 	"github.com/radovskyb/watcher"
-	"log"
 	"os"
 	"os/user"
 	"regexp"
@@ -16,7 +15,7 @@ func main() {
 	if _, err := os.Stat("./test_folder"); errors.Is(err, os.ErrNotExist) {
 		err := os.Mkdir("./test_folder", os.ModePerm)
 		if err != nil {
-			log.Println(err)
+			println(err)
 		}
 	}
 
@@ -35,7 +34,7 @@ func main() {
 	// If SetMaxEvents is not set, the default is to send all events.
 	//w.SetMaxEvents(1)
 
-	// Only notify rename and move events.
+	// Only notify these events.
 	w.FilterOps(watcher.Rename, watcher.Move, watcher.Create, watcher.Write)
 
 	// Only files that match the regular expression during file listings
@@ -48,41 +47,27 @@ func main() {
 			select {
 			case event := <-w.Event:
 				if event.Op == 0 {
-					log.Println(event) // Print the event's info.
 
 					if err := cp.Copy(desktop, "./test_folder/desktop"); err != nil {
-						log.Println(err)
+						println(err)
 					}
 				}
 
 			case err := <-w.Error:
-				log.Println(err)
+				println(err)
 			case <-w.Closed:
 				return
 			}
 		}
 	}()
 
-	// Watch this folder for changes.
-
-	// Watch test_folder recursively for changes.
-	if err := w.AddRecursive("./test_folder"); err != nil {
-		log.Fatalln(err)
-	}
-
 	// Watch test_folder recursively for changes.
 	if err := w.AddRecursive(desktop); err != nil {
-		log.Fatalln(err)
+		println(err)
 	}
-
-	// Print a list of all of the files and folders currently
-	// being watched and their paths.
-	//for path, f := range w.WatchedFiles() {
-	//	fmt.Printf("%s: %s\n", path, f.Name())
-	//}
 
 	// Start the watching process - it'll check for changes every 100ms.
 	if err := w.Start(time.Millisecond * 100); err != nil {
-		log.Fatalln(err)
+		println(err)
 	}
 }
